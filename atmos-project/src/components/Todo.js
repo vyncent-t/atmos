65c50cae5b73c625db9cf2e920df0dbbfb3d8175
Vincent T <vyncode@gmail.com>
2021-09-16 14:21:07 -0400

    practice work

---
+++ atmos-project/src/components/Todo.js
@@ -0,0 +1,12 @@
+function Todo() {
+    return (
+        <div className="praccard">
+            <h2>Title</h2>
+            <div className="pracactions">
+                <button>Delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todo;
