2dcc287922efff5b9cc6d45258c987b4560c01fb
Vincent T <vyncode@gmail.com>
2021-11-14 18:58:27 -0500

    return

--- src/components/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import styles from './MainMenuStyles.module.css'
-
-function Backdrop(props) {
-    return (
-        <div className={styles.backdrop} onClick={props.onCancel} />
-    )
-}
-
-export default Backdrop
