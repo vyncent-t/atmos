b6cf84da4a841d32be6d4c81079123581d7e71a0
Vincent T <vyncode@gmail.com>
2021-11-14 14:39:21 -0500

    dot env

--- src/store/SpotifyState.js
+++ src/store/SpotifyState.js
@@ -1,5 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+require('dotenv').config()
+
+
 const initialSpotifyState = {
     spotifyAuthLink: 'https://accounts.spotify.com/authorize?client_id=50885eb87ce14757bdde10e7fb01f91a&response_type=code&redirect_uri=https://vyncent-t.github.io/atmos&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state',
     isSpotifyAuth: false,
